122848cf6877f27122387fed98bc05f750ec870a
VagTsop <vatsop52@gmail.com>
2022-02-26 21:50:16 +0200

    create menu screen

--- test-project-client/src/app/app.module.ts
+++ test-project-client/src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './my-components/header/header.component';
 import { ListComponent } from './my-components/list/list.component';
+import { MenuComponent } from './my-components/menu/menu.component';
 import { SharedModule } from './shared.module';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ListComponent,
+    MenuComponent
   ],
   imports: [
     SharedModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
